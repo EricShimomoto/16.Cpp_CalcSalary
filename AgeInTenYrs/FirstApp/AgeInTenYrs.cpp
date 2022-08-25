8daff3a486ff8b2cf7d717807223defb9a9e64f5
Eric Shimomoto <91641043+EricShimomoto@users.noreply.github.com>
2022-08-25 15:25:30 +0900

    First and probably last commit

---
+++ AgeInTenYrs/FirstApp/AgeInTenYrs.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cout << "Hello World!\n";
+    cout << "What is your age? ";
+    cin >> age;
+    cout << "\nYou will be " << age + 10 << " in 10 years" << endl;
+    system("pause>0");//It takes out the bunch of text
+}
